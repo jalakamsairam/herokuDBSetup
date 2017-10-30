9ee9d57d08cc11d2823720926bc67c3722d13fd5
jalakamsairam <sairam.jalakamdevarajulu@mavs.uta.edu>
2017-10-30 14:46:28 -0700

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name) VALUES ('Farley');
+INSERT INTO cats (name) VALUES ('Moose');
+INSERT INTO cats (name, sleepy) VALUES ('sergeant', true);
+INSERT INTO cats (name, sleepy) VALUES ('Snikers', true);
+INSERT INTO cats (name, sleepy) VALUES ('Mr. Snuggles', true);
+INSERT INTO cats (name) VALUES ('Bob');
